46cfd010586e85b7866a884917f3722cadf068cb
sora1 <gonzalezevans05@gmail.com>
2022-11-26 21:56:02 -0500

    super cambios

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/estilos.css">
+    <link rel="stylesheet" href="/css/FooterEfect.css">
+    <link rel="stylesheet" href="/css/Footer.css">
+    <link rel="stylesheet" href="/css/menutest.css">
+    <link rel="stylesheet" href="/css/carruselp.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body class="holy-grail">
+    <header>
+        <?php
+        include("menu_footer/menumain.html");
+        ?>
+    </header>
+
+    <div class="holy-grail-body">
+
+        <section class="holy-grail-content">
+        <?php
+        include("contenido/CarruselPrincipal.html");
+        ?>
+        </section>
+
+        
+
+    </div>
+
+    <footer>
+        <?php
+        include("menu_footer/footerefects.html");
+        ?>
+    </footer>
+    <script src="/JS/Carrusel.js"></script>
+</body>
+
+</html>
