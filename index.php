b6ee52c6ba851d8980c2744713231f2a02b3099b
sora1 <gonzalezevans05@gmail.com>
2022-12-02 22:22:38 -0500

    SUPER CAMBIOS

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
     <link rel="stylesheet" href="/css/carruselp.css">
     <link rel="stylesheet" href="/css/contenidoP.css">
     <link rel="stylesheet" href="/css/contenindex.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <title>Document</title>
+    <title>LISMI CAKE’S</title>
 </head>
 
 <body class="holy-grail">
